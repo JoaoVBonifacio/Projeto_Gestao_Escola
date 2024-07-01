9538032292ed92b83934cd41de4ed34c41b7366c
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 16:24:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from google.oauth2 import id_token
-from google.auth.transport import requests as google_requests
-import os
 
 app = Flask(__name__)
 
