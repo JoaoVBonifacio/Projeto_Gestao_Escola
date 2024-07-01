a1bad412b2fba42c00707aa7c1c4a6f73cad7091
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 14:48:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
-from google.oauth2 import id_token
-from google.auth.transport import requests
 
 app = Flask(__name__)
-
-GOOGLE_CLIENT_ID = "1078026969554-77vn7s1tv4avm8lk3ugobeofbqg0dtk8.apps.googleusercontent.com"
 
 @app.route('/')
 def index():
