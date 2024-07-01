d706f7a9613cc51fde9eb905d1615ae94931a1d5
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 20:07:06 -0300

    Projeto Gestão Escola
    v0.0.5.7

--- app.py
+++ app.py
@@ -5,22 +5,29 @@ import os
 
 app = Flask(__name__)
 
-GOOGLE_CLIENT_ID = os.environ.get('1078026969554-77vn7s1tv4avm8lk3ugobeofbqg0dtk8.apps.googleusercontent.com')
+GOOGLE_CLIENT_ID = os.environ.get('1078026969554-2snnqosr4kkfu8e5hbdmp7f4keqacmj4.apps.googleusercontent.com', '1078026969554-2snnqosr4kkfu8e5hbdmp7f4keqacmj4.apps.googleusercontent.com')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
     token = request.json.get('id_token')
     try:
+        # Specify the CLIENT_ID of the app that accesses the backend
         idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), GOOGLE_CLIENT_ID)
+
+        # ID token is valid. Get the user's Google Account ID from the decoded token.
         userid = idinfo['sub']
         email = idinfo['email']
         name = idinfo['name']
+        # You can store user information in session or database
+        # For example, session['user'] = {'userid': userid, 'email': email, 'name': name}
+        
         return jsonify(success=True, email=email, name=name)
     except ValueError:
+        # Invalid token
         return jsonify(success=False), 401
 
 if __name__ == '__main__':
