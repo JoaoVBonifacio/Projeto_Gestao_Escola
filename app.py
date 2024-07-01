73649d0ec24efa5361a981c62b31a3a3a3ccfdd8
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 14:43:33 -0300

    Projeto Gestão Escola
    v0.0.5.4

--- app.py
+++ app.py
@@ -10,21 +10,5 @@ GOOGLE_CLIENT_ID = "1078026969554-77vn7s1tv4avm8lk3ugobeofbqg0dtk8.apps.googleus
 def index():
     return render_template('index.html')
 
-@app.route('/login', methods=['POST'])
-def login():
-    token = request.json.get('id_token')
-    try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), GOOGLE_CLIENT_ID)
-        userid = idinfo['sub']
-        # Aqui você pode verificar se o usuário está no seu banco de dados e criar uma sessão
-        return jsonify(success=True)
-    except ValueError:
-        # Token inválido
-        return jsonify(success=False), 401
-
-@app.route('/dashboard')
-def dashboard():
-    return "Bem-vindo ao Dashboard!"
-
 if __name__ == '__main__':
     app.run(debug=True)
