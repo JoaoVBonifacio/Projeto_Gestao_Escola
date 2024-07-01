90cda85560d8b7ccd26495825fda6c35d9ff168d
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 16:21:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,6 @@ import os
 
 app = Flask(__name__)
 
-GOOGLE_CLIENT_ID = os.environ.get('1078026969554-77vn7s1tv4avm8lk3ugobeofbqg0dtk8.apps.googleusercontent.com', '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')
-
 @app.route('/')
 def index():
     return render_template('index.html')
