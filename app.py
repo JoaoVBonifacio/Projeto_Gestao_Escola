5af11295803e3a2fe920e98083526e3816a7369a
JoaoVBonifácio <jboni2014@gmail.com>
2024-07-01 21:06:38 -0300

    Projeto Gestão Escola
    v0.0.5.8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask import Flask, render_template, request, jsonify
 from google.oauth2 import id_token
 from google.auth.transport import requests as google_requests
 import os
@@ -15,19 +15,12 @@ def index():
 def login():
     token = request.json.get('id_token')
     try:
-        # Specify the CLIENT_ID of the app that accesses the backend
         idinfo = id_token.verify_oauth2_token(token, google_requests.Request(), GOOGLE_CLIENT_ID)
-
-        # ID token is valid. Get the user's Google Account ID from the decoded token.
         userid = idinfo['sub']
         email = idinfo['email']
         name = idinfo['name']
-        # You can store user information in session or database
-        # For example, session['user'] = {'userid': userid, 'email': email, 'name': name}
-        
         return jsonify(success=True, email=email, name=name)
     except ValueError:
-        # Invalid token
         return jsonify(success=False), 401
 
 if __name__ == '__main__':
